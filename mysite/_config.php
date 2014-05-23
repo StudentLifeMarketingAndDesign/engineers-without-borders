4397c7097a4d47ca777ab3eba00c97a4e3dba178
Dustin Quam <quamsta@gmail.com>
2014-05-23 11:51:00 -0500

    converted to a composer project

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,15 +4,15 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = '';
+$database = 'ewb';
 
 require_once('conf/ConfigureFromEnv.php');
 
 MySQLDatabase::set_connection_charset('utf8');
 
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
-
+// Set the site locale
+i18n::set_locale('en_US');
+Director::set_environment_type("live");
 // Enable nested URLs for this site (e.g. page/sub-page/)
-if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+GD::set_default_quality(85);
