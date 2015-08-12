2b803d2806297cc57c8fe3d8634b80cb7f9620b7
javeriaaali <javeria-ali@uiowa.edu>
2015-08-12 13:41:08 -0500

    Update _config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -16,3 +16,7 @@ i18n::set_locale('en_US');
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 GD::set_default_quality(85);
+
+if(Director::isLive()) {
+	Director::forceSSL(array('/^Security/','/^admin/'));
+}
